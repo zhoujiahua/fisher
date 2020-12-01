9967ec96863bbfe93f799eac211c5b396c7377ec
jerry <jiahuasir@gmail.com>
2020-12-01 14:44:44 +0800

    http request code

--- http.py
+++ http.py
@@ -4,20 +4,10 @@ import requests
 
 
 class HTTP:
+    # 经典类和新式类
     @staticmethod
     def get(url, return_json=True):
         r = requests.get(url)
         if r.status_code != 200:
             return {} if return_json else ''
         return r.json() if return_json else r.text
-
-        # if r.status_code == 200:
-        #     if return_json:
-        #         return r.json()
-        #     else:
-        #         return r.text
-        # else:
-        #     if return_json:
-        #         return {}
-        #     else:
-        #         return ''
